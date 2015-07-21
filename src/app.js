7552ca3935fc37872dc1cde0ce81004127671a42
Martin Herløv Andersen <mha@miracleas.dk>
2015-07-21 15:45:56 +0200

    Start work on the dialog

--- src/app.js
+++ src/app.js
@@ -1,13 +1,25 @@
 export class App {
 	selectedName = 'Martin';
+	showing = false;
 	names = ['Martin','Anders','Knud'];
 	
 	constructor(){
+	}
+	
+	showDialog = () => {
+		this.showing = true;
+	}
+
+	
+	closeDialog = () => {
+		this.showing = false;
+	}
+
+	saveDialogResult = () => {
 		
 	}
 	
 	selectName = (name) => {
-		//self = this;
 		this.selectedName = name;
 	}
 	
